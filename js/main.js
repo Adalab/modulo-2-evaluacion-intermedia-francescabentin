958f3df073e60b8bf712367a61f5058359b47dd6
Francesca Bentin <francescabentin@gmail.com>
2023-02-14 10:39:36 +0100

    add if function

--- js/main.js
+++ js/main.js
@@ -13,11 +13,34 @@ function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
 }
 
-const getRandom = getRandomNumber(100);
-console.log(getRandom);
+const random = getRandomNumber(100);
+console.log(random);
 
 // add click event to button
 
 button.addEventListener("click", () => {
-    console.log(`el numero es ${guessNumber.value}`);
-});
+
+const minNumber = 1;
+const maxNumber = 100;
+const number = (guessNumber.value);
+let counter = 0;
+
+if (number > random ) {
+    textHint.innerHTML = "Demasiado Alto";
+}
+    else if (number < random ) {
+    textHint.innerHTML = "Demasiado bajo";
+}
+    else if (number === random ) {
+    textHint.innerHTML = "Has ganado campeona!!!";
+}
+    else if (number < minNumber || number > maxNumber) {
+    textHint.innerHTML = "El número debe estar entre 1 y 100";
+}
+    else if (isNaN(number)) {
+    textHint.innerHTML = "Escribir un numero";
+    }
+
+}); 
+
+
