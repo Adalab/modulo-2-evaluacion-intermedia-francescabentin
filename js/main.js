6a6e74ed51ed3e2987d2fafeeb37c81607be7d3a
Francesca Bentin <francescabentin@gmail.com>
2023-02-21 17:16:35 +0100

    make basic corrections

--- js/main.js
+++ js/main.js
@@ -2,10 +2,11 @@
 
 // html elements
 
-const guessNumber = document.querySelector(".js-inputNumber");
+const inputNumber = document.querySelector(".js-inputNumber");
 const button = document.querySelector(".js-btn");
 const textHint = document.querySelector(".js-hint");
 const textTried = document.querySelector(".js-tried");
+const reset = document.querySelector(".js-btn-reset");
 
 // random number function
 
@@ -13,38 +14,72 @@ function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
 }
 
-const random = getRandomNumber(100);
+let random = getRandomNumber(100);
 console.log(random);
 
-// add click event to button
+
+// write function
+
+function writeMsg(msg) {
+    textHint.innerHTML = msg;
+}
+
+// incrementNumber and paint function
+
+    const incrementNumber = (number) => {
+        if (number !== random) {
+            counter++;
+        }
+        textTried.innerHTML = `Numero de intentos: ${counter}`
+    }
+
+// Compare number function
+
+function compareNumber() {
+    const number = parseInt(inputNumber.value);
+
+    if (number > 100 || number < 1) {
+        writeMsg("El número debe estar entre 1 y 100");
+    }
+    else if (number > random) {
+        writeMsg("Demasiado Alto");
+    }
+    else if (number < random) {
+        writeMsg("Demasiado bajo");
+    }
+    else if (number === random) {
+        writeMsg("Has ganado campeona!!!");
+    }
+    else if (isNaN(number)) {
+        writeMsg("Escribir un numero");
+    }
+
+};
+
+// handleClickBtn function
+
 let counter = 0;
 
-button.addEventListener("click", (event) => {
-event.preventDefault();
-const number = parseInt(guessNumber.value);
-counter++;
+function handleClickBtn() {
 
-if (number > 100 || number < 1 ) {
-    textHint.innerHTML = "El número debe estar entre 1 y 100";
-}
-else if (number > random ) {
-    textHint.innerHTML = "Demasiado Alto";
-    
-}
-    else if (number < random ) {
-    textHint.innerHTML = "Demasiado bajo";
-    
-}
-    else if (number === random ) {
-    textHint.innerHTML = "Has ganado campeona!!!";
-    counter = 0;
-}
-    else if (isNaN(number)) {
-    textHint.innerHTML = "Escribir un numero";
-}
+    compareNumber();
+    incrementNumber();
    
+}
 
-textTried.innerHTML = `Numero de intentos: ${counter}`
-});
+// reset btn
+    function resetAll() {
+    counter = 0;
+    random = getRandomNumber(100);
+    guessNumber.value = "";
+    writeMsg("Escribe un numero para jugar");
+     textTried.innerHTML = `Numero de intentos: ${counter}`;
+    }
+
+// button event listener
+
+button.addEventListener("click", handleClickBtn);
+reset.addEventListener('click', resetAll);
+
 
 
