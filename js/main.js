00c6663bc26a17391d703e3d0819b972b7079759
Francesca Bentin <francescabentin@gmail.com>
2023-02-21 18:34:16 +0100

    final changes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 "use strict";
 
 // html elements
-
 const inputNumber = document.querySelector(".js-inputNumber");
 const button = document.querySelector(".js-btn");
 const textHint = document.querySelector(".js-hint");
@@ -9,7 +8,6 @@ const textTried = document.querySelector(".js-tried");
 const reset = document.querySelector(".js-btn-reset");
 
 // random number function
-
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
 }
@@ -19,22 +17,19 @@ console.log(random);
 
 
 // write function
-
 function writeMsg(msg) {
     textHint.innerHTML = msg;
 }
 
 // incrementNumber and paint function
-
-    const incrementNumber = (number) => {
-        if (number !== random) {
-            counter++;
-        }
-        textTried.innerHTML = `Numero de intentos: ${counter}`
+const incrementNumber = (number) => {
+    if (number !== random) {
+        counter++;
     }
+    textTried.innerHTML = `Numero de intentos: ${counter}`
+}
 
-// Compare number function
-
+// compare number function
 function compareNumber() {
     const number = parseInt(inputNumber.value);
 
@@ -53,36 +48,31 @@ function compareNumber() {
     else if (isNaN(number)) {
         writeMsg("Escribir un numero");
     }
-
 };
 
 // handleClickBtn function
-
 let counter = 0;
 
 function handleClickBtn() {
     let number = parseInt(inputNumber.value);
 
     if (!isNaN(number)) {
-    compareNumber();
-    incrementNumber(number);
+        compareNumber();
+        incrementNumber(number);
     }
-
     inputNumber.value = "";
-   
 }
 
 // reset btn
-    function resetAll() {
+function resetAll() {
     counter = 0;
     random = getRandomNumber(100);
     inputNumber.value = "";
     writeMsg("Escribe un numero para jugar");
-     textTried.innerHTML = `Numero de intentos: ${counter}`;
-    }
+    textTried.innerHTML = `Numero de intentos: ${counter}`;
+}
 
-//keydown event
-
+// keydown event
 function pressEnter(event) {
     if (event.key === 'Enter') {
         handleClickBtn();
@@ -90,7 +80,6 @@ function pressEnter(event) {
 }
 
 // button event listener
-
 document.addEventListener("keydown", pressEnter);
 button.addEventListener("click", handleClickBtn);
 reset.addEventListener('click', resetAll);
